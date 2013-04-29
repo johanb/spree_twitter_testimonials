171eb8cb9dfc82174cce4fe5390c8a82141cf37c
Johan Bruning <johan.h.bruning@gmail.com>
2013-04-29 20:25:47 +0200

    Add original directive to deface override

--- app/overrides/add_tweets_to_footer.rb
+++ app/overrides/add_tweets_to_footer.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "add_tweets_to_footer",
                      :insert_top => "section#bottom",
-                     :partial => 'spree/twitter_testimonials/tweets')
+                     :partial => 'spree/twitter_testimonials/tweets',
+                     :original => 'fa03ec3abd77f1ea58b6696bf9ff7ea21bf61400')
